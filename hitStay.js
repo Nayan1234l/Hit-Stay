9aa70894f14f3352bf1f383d694092da68f5dfb9
Nayan1234l <ali15-13943@diu.edu.bd>
2024-05-14 16:12:27 +0600

    showing-opponent-the-card-he-required

--- hitStay.js
+++ hitStay.js
@@ -7,6 +7,7 @@ var secret, deck, canHit = true;//allows the player to draw while yourSum<=21
 window.onload = () => {
     constructDeck()
     shufflingDeck()
+    startGame()
 }
 
 
@@ -17,7 +18,7 @@ const constructDeck = () => {
     //for each type we go through the whole values and push it to the deck where we play 
     for (let i = 0; i < types.length; i++) {
         for (let j = 0; j < values.length; j++) {
-            deck.push(`${values[j]} - ${types[i]}`)//got here 52 cards
+            deck.push(`${values[j]}-${types[i]}`)//got here 52 cards
         }
     }
     // console.log(deck)
@@ -29,4 +30,51 @@ const shufflingDeck = () => {
         [deck[randomCard], deck[card]] = [deck[card], deck[randomCard]]
     }
     console.log(deck)
+}
+
+
+const startGame = () => {
+    secret = deck.pop()
+    opponentSum += getValue(secret);
+    opponentTekkaCount += checkTekka(secret);
+    // console.log(secret)
+    // console.log(opponentSum)
+    while (opponentSum < 17) {
+        //create img tag to give the opponent if this condition match
+        let imgCard = document.createElement("img");
+        let card = deck.pop()
+        // imgCard.src=`./cards/ ${card} .png`
+        imgCard.src = "./cards/" + card + ".png"
+        opponentSum += getValue(card)
+        opponentTekkaCount += checkTekka(card)
+        document.getElementById("opponent-cards").append(imgCard)
+    }
+    console.log(opponentSum)
+}
+
+const getValue = (card) => {
+    let data = card.split('-')
+    let value = data[0]
+    //if its a char
+    if (isNaN(value)) {
+        if (value === 'A') {
+            return 11;//because A==11 tekka
+        }
+        else {
+            return 10;//others J Q K ==10
+        }
+    }
+    else {
+        return parseInt(value);//otherwise return the value itself
+    }
+}
+
+const checkTekka = (card) => {
+    if (card[0] === 'A') {
+        return 1;
+    }
+    else {
+
+        return 0;
+    }
 }
